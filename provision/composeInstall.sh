c4b2e3346bdb64cbc89bbdfb4f76bc93bf20c049
Kotov Aleksandr <kotov.alexandr@gmail.com>
2024-08-14 17:58:31 +0300

    Add Ping Playbook

---
+++ provision/composeInstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install ca-certificates curl
+sudo apt install docker.io
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y
+sudo apt install docker-compose-plugin -y
+
+sudo usermod -aG docker ${USER}
+init 6
